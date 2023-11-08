040f78dc4cb7cb8c1c86cd3fe5f83c76ee0ff074
maxbond <ceo@rf.ru>
2023-11-08 21:48:16 +0300

    new form created

--- src/App.js
+++ src/App.js
@@ -24,6 +24,7 @@ export default function App() {
     <div className='app'>
       <div className='sidebar'>
         <FriendsList />
+        <FormAddFriend />
       </div>
     </div>
   );
@@ -57,7 +58,24 @@ function Friend({friend}) {
         </p>
       )}
       {friend.balance === 0 && <p>You and {friend.name} are even</p>}
-      <button className='button'>Select</button>
+      <Button>Select</Button>
     </li>
+  );
+}
+
+function Button({children}) {
+  return <button className='button'>{children}</button>;
+}
+
+function FormAddFriend() {
+  return (
+    <form className='form-add-friend'>
+      <label> 👫Friend name</label>
+      <input type='text' />
+
+      <label> 🌄 Image URL</label>
+      <input type='text' />
+      <Button>Select</Button>
+    </form>
   );
 }
