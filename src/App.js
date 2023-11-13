1cd0efa0464d9d93038427712fd7c8223827cfe7
maxbond <ceo@rf.ru>
2023-11-13 23:45:59 +0300

    added functionality to AddFriend form

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ const initialFriends = [
   {
     id: 499476,
     name: 'Anthony',
-    image: 'https://i.pravatar.cc/48?u=499476',
+    image: 'https://i.pravatar.cc/48z?u=499476',
     balance: 0,
   },
 ];
@@ -84,13 +84,43 @@ function Friend({friend}) {
 }
 
 function FormAddFriend() {
+  const [name, setName] = useState('');
+  const [image, setImage] = useState('https://i.pravatar.cc/48z');
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    if (!name || !image) return;
+
+    const id = crypto.randomUUID();
+    const newFriend = {
+      id,
+      name,
+      image: `${image}?=${id}`,
+      balance: 0,
+    };
+
+    console.log(newFriend);
+
+    setName('');
+    setImage('https://i.pravatar.cc/48z');
+  }
+
   return (
-    <form className='form-add-friend'>
+    <form className='form-add-friend' onSubmit={handleSubmit}>
       <label> 👫Friend name</label>
-      <input type='text' />
+      <input
+        type='text'
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
 
       <label> 🌄 Image URL</label>
-      <input type='text' />
+      <input
+        type='text'
+        value={image}
+        onChange={(e) => setImage(e.target.value)}
+      />
       <Button>Add</Button>
     </form>
   );
