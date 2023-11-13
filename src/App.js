4afe8d9897714fd8ee6c8c1674b77af9a5ec0c79
maxbond <ceo@rf.ru>
2023-11-14 00:45:06 +0300

    new friend added and rendered

--- src/App.js
+++ src/App.js
@@ -30,17 +30,22 @@ function Button({children, onClick}) {
 }
 
 export default function App() {
+  const [friends, setFriends] = useState(initialFriends);
   const [showAddFriend, setShowAddFriend] = useState(false);
 
   function handleShowAddFriend() {
     setShowAddFriend((show) => !show);
   }
+  function handleAddFriend(friend) {
+    setFriends((friends) => [...friends, friend]);
+    setShowAddFriend(false);
+  }
 
   return (
     <div className='app'>
       <div className='sidebar'>
-        <FriendsList />
-        {showAddFriend && <FormAddFriend />}
+        <FriendsList friends={friends} />
+        {showAddFriend && <FormAddFriend onAddFriend={handleAddFriend} />}
         <Button onClick={handleShowAddFriend}>
           {showAddFriend ? 'Close' : 'Add friend'}
         </Button>
@@ -50,9 +55,7 @@ export default function App() {
   );
 }
 
-function FriendsList() {
-  const friends = initialFriends;
-
+function FriendsList({friends}) {
   return (
     <ul>
       {friends.map((friend) => (
@@ -83,9 +86,9 @@ function Friend({friend}) {
   );
 }
 
-function FormAddFriend() {
+function FormAddFriend({onAddFriend}) {
   const [name, setName] = useState('');
-  const [image, setImage] = useState('https://i.pravatar.cc/48z');
+  const [image, setImage] = useState('https://i.pravatar.cc/48?');
 
   function handleSubmit(e) {
     e.preventDefault();
@@ -100,10 +103,10 @@ function FormAddFriend() {
       balance: 0,
     };
 
-    console.log(newFriend);
+    onAddFriend(newFriend);
 
     setName('');
-    setImage('https://i.pravatar.cc/48z');
+    setImage('https://i.pravatar.cc/48?');
   }
 
   return (
